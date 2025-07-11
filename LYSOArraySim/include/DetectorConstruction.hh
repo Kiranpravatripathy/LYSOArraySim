c9a00306ea1d9501262b1f062b4069d5e2fa0bcd
Kiranpravatripathy <136947936+Kiranpravatripathy@users.noreply.github.com>
2025-07-11 10:13:30 +0530

    Create DetectorConstruction.hh

---
+++ LYSOArraySim/include/DetectorConstruction.hh
@@ -0,0 +1,11 @@
+#pragma once
+#include "G4VUserDetectorConstruction.hh"
+#include "globals.hh"
+
+class DetectorConstruction : public G4VUserDetectorConstruction {
+public:
+    DetectorConstruction() = default;
+    virtual ~DetectorConstruction() = default;
+
+    virtual G4VPhysicalVolume* Construct() override;
+};
